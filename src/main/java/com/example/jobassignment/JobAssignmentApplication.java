ed5598e5b8c20e190bc5dee8cc9f02149c028645
seaking110 <seaking110@naver.com>
2025-05-13 15:26:50 +0900

    first commit

---
+++ src/main/java/com/example/jobassignment/JobAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobAssignmentApplication.class, args);
+    }
+
+}
